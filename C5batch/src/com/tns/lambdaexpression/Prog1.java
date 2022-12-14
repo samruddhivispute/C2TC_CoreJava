15d28b44d43c594b6367ae2c67db4da07141bfa0
samru <samru@MSI>
2022-12-14 20:15:47 +0530

    ninth commit

--- C5batch/src/com/tns/lambdaexpression/Prog1.java
+++ C5batch/src/com/tns/lambdaexpression/Prog1.java
@@ -2,7 +2,7 @@ package com.tns.lambdaexpression;
 
 interface Lambda
 {
-	abstract public void disp(int x);
+	abstract public int disp();
 	
 }
 
@@ -10,11 +10,10 @@ public class Prog1 {
 
 	public static void main(String[] args)
 	{
-		Lambda obj=(int x)->{
-			System.out.println("value of x is: "+x);
-		};
-		obj.disp(56);
+		Lambda obj=()->56+67*45-456/56;
+			System.out.println(obj.disp());
+		}
 	
 	}
 
-}
+
